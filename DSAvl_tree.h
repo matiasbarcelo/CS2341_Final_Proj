a0e738d28a6e9ba55df23d6d9604bf2ea262d835
Matias Ezequiel Barcelo Treiyer <mbarcelotreiyer@genuse54.engr.smu.edu>
2024-04-15 01:54:00 -0500

    Implemented DSAvl_tree with set values, added CTests which were all passed.

---
+++ DSAvl_tree.h
@@ -0,0 +1,487 @@
+#ifndef DSAVL_TREE_H
+#define DSAVL_TREE_H
+
+using namespace std;
+
+/*
+    The project instructions indicated that I can use implementations from notes, and the CS 2341 class GitHub is considered a collection of "class notes";
+    so, I copied and pasted the implementation from the Data Structures GitHub DSAvl_tree.h so I wasn't rewriting everything and modified it for the use of this project.
+*/
+
+template <typename Comparable, typename Comparable2>
+class DSAvl_tree{    
+    private:
+
+        struct AvlNode{
+            Comparable key;
+            set<Comparable2> values;
+            AvlNode* left;
+            AvlNode* right;
+            int height;
+
+            // without any value
+            AvlNode(const Comparable &theKey, AvlNode *lt, AvlNode *rt, int h)
+            : key{theKey}, left{lt}, right{rt}, height{h} {}
+
+            // with a value
+            AvlNode(const Comparable &theKey, Comparable2 &theValue, AvlNode *lt, AvlNode *rt, int h)
+            : key{theKey}, left{lt}, right{rt}, height{h} {
+                values.insert(theValue);
+            }
+        };
+
+        AvlNode* root;
+
+        static const int ALLOWED_IMBALANCE = 1;
+
+    public:
+
+    // Default constructor
+    DSAvl_tree() : root{nullptr}
+    {
+    }
+
+    // Rule-of-3 Part 1: Copy constructor
+    DSAvl_tree(const DSAvl_tree &rhs) : root{nullptr}
+    {
+        root = clone(rhs.root);
+    }
+
+    // Rule-of-3 Part 2: Destructor
+    ~DSAvl_tree()
+    {
+        makeEmpty();
+    }
+
+    // Rule-of-3 Part 3: Copy assignment operator
+    DSAvl_tree &operator=(const DSAvl_tree &rhs)
+    {
+        if (this != &rhs)
+        {
+            makeEmpty();
+            root = clone(rhs.root);
+        }
+
+        return *this;
+    }
+
+    // Move semantics could be implemented here.
+
+    /**
+     * Returns true if x is found in the tree.
+     */
+    bool contains(const Comparable &x) const
+    {
+        return contains(x, root);
+    }
+
+    /**
+     * Test if the tree is logically empty.
+     * Return true if empty, false otherwise.
+     */
+    bool isEmpty() const
+    {
+        return root == nullptr;
+    }
+
+    /**
+     * Print the tree structure.
+     */
+    void prettyPrintTree() const
+    {
+        prettyPrintTree("", root, false);
+    }
+
+    /**
+     * Returns a vector of all keys
+    */
+   vector<Comparable> getKeys(){
+        vector<Comparable> theVector;
+        getKeys(root, theVector);
+        return theVector;
+   }
+
+    /**
+     * Returns a vector of all values for a key
+    */
+   vector<Comparable2> getValues(Comparable theKey){
+        vector<Comparable2> theVector;
+        set<Comparable2>* theSet;
+        getValues(root, theKey, theSet);
+        for (auto value : *theSet){
+            theVector.push_back(value);
+        }
+        return theVector;
+   }
+
+    /**
+     * Make the tree empty.
+     */
+    void makeEmpty()
+    {
+        makeEmpty(root);
+    }
+
+    /**
+     * Insert x into the tree; duplicates are ignored.
+     */
+    void insert(const Comparable &x)
+    {
+        insert(x, root);
+    }
+
+    /**
+     * Inserts y value into the tree on x node. IMPLEMENTED BY ME
+    */
+   void insertValue(const Comparable &x, Comparable2 &y)
+   {
+        insertValue(x, y, root);
+   }
+
+    /**
+     * Remove x from the tree. Nothing is done if x is not found.
+     */
+    void remove(const Comparable &x)
+    {
+        remove(x, root);
+    }
+
+#ifdef DEBUG
+    /**
+     * Check if the tree is balanced and that the height of the nodes is correct.
+     * Throws an exception if the tree is not balanced or the height is wrong.
+     * This function is not necessary in production code since the tree is always balanced.
+     * It is only compiled when DEBUG is defined.
+     */
+
+    int check_balance()
+    {
+        return check_balance(root);
+    }
+#endif
+
+private:
+    /**
+     * Internal method to insert comparable into a subtree.
+     * x is the comparable item to insert.
+     * t is the node that roots the subtree.
+     * Set the new root of the subtree.
+     */
+    void insert(const Comparable &x, AvlNode *&t)
+    {
+        if (t == nullptr)
+        {
+            t = new AvlNode{x, nullptr, nullptr, 0};
+            return; // a single node is always balanced
+        }
+
+        if (x < t->key)
+            insert(x, t->left);
+        else if (t->key < x)
+            insert(x, t->right);
+        else
+        {
+        } // Duplicate; do nothing
+
+        // This will call balance on the way back up the tree. It will only balance
+        // once at node the where the tree got imbalanced (called node alpha in the textbook)
+        // and update the height all the way back up the tree.
+        balance(t);
+    }
+
+    void insertValue(const Comparable &x, const Comparable2 &y, AvlNode *&t){
+        if (t == nullptr){
+            return;
+        }
+
+        if (x == t->key){
+            t->values.insert(y);
+        }
+        else if (x < t->key)
+            insertValue(x, y, t->left);
+        else
+            insertValue(x, y, t->right);
+    }
+
+    /**
+     * Internal method to remove from a subtree.
+     * x is the item to remove.
+     * t is the node that roots the subtree.
+     * Set the new root of the subtree.
+     */
+    void remove(const Comparable &x, AvlNode *&t)
+    {
+        throw std::runtime_error("Not implemented yet!");
+        // same as in a binary search tree
+
+        // don't forget to balance the AVL tree after the deletion!
+    }
+
+    /**
+     * Internal method to check if x is found in a subtree rooted at t.
+     */
+    bool contains(const Comparable &x, AvlNode *t) const
+    {
+        if (t == nullptr)
+            return false;
+
+        if (x == t->key)
+            return true; // Element found.
+        else if (x < t->key)
+            return contains(x, t->left);
+        else
+            return contains(x, t->right);
+    }
+
+    /**
+     * Internal method to make subtree empty.
+     */
+    void makeEmpty(AvlNode *&t)
+    {
+        if (t == nullptr)
+            return;
+
+        makeEmpty(t->left);
+        makeEmpty(t->right);
+        delete t;
+        t = nullptr;
+    }
+
+    /**
+     * Internal method to clone subtree.
+     */
+    AvlNode *clone(AvlNode *t) const
+    {
+        if (t == nullptr)
+            return nullptr;
+
+        return new AvlNode{t->key, t->values, clone(t->left), clone(t->right), t->height};
+    }
+
+    /**
+     * Pretty print the tree structure
+     * Uses preorder traversal with R and L swapped (NRL)
+     *
+     * Modified from: https://stackoverflow.com/questions/36802354/print-binary-tree-in-a-pretty-way-using-c
+     */
+    void prettyPrintTree(const std::string &prefix, const AvlNode *node, bool isRight) const
+    {
+        if (node == nullptr)
+            return;
+
+        std::cout << prefix;
+        // Note: this uses unicode characters for the tree structure. They might not print correctly on
+        // all systems (Windows!?!) and all types of output devices.
+        std::cout << (isRight ? "├──" : "└──");
+        // print the value of the node
+        std::cout << node->key << std::endl;
+
+        // enter the next tree level - left and right branch
+        prettyPrintTree(prefix + (isRight ? "│   " : "    "), node->right, true);
+        prettyPrintTree(prefix + (isRight ? "│   " : "    "), node->left, false);
+    }
+
+    /**
+     * This uses LNR to return a vector containing all the keys
+    */
+   void getKeys(AvlNode* node, vector<Comparable>& theVector)
+   {    
+        // if the node is a nullptr just return
+        if(node == nullptr){
+            return;
+        }
+
+        // L
+        getKeys(node->left, theVector);
+        // N
+        theVector.push_back(node->key);
+        // R
+        getKeys(node->right, theVector);
+   }
+
+   void getValues(AvlNode* node, Comparable theKey, set<Comparable2> *&theSet){
+        if (node == nullptr){
+            return;
+        }
+
+        if (node->key == theKey){
+            theSet = &node->values; // Key found, writing theSet is now the same as the key node's value field
+        }
+        else if (theKey < node->key){
+            getValues(node->left, theKey, theSet);
+        }
+        else{
+            getValues(node->right, theKey, theSet);
+        }
+   }
+
+    // Balancing: AVL Rotations
+
+    /**
+     * Return the height of node t or -1 if nullptr.
+     */
+    int height(AvlNode *t) const
+    {
+        return t == nullptr ? -1 : t->height;
+    }
+
+    /**
+     * 1. Performs rotations if the the the difference of the height stored in t's two child nodes
+     *    more than ALLOWED_IMBALANCE.
+     * 2. Updates the height information of the note t.
+     *
+     * Assumes that the high information in the child nodes is correct. This is guaranteed by calling
+     * balance() recursively from the inserted node up to the tree node (see insert()). Rotations will
+     * only be performed for node alpha (parent of the parent of the inserted node). For all other nodes,
+     * only the height will be updated.
+     */
+    void balance(AvlNode *&t)
+    {
+        // special case: empty tree
+        if (t == nullptr)
+            return;
+
+        if (height(t->left) - height(t->right) > ALLOWED_IMBALANCE) // unbalancing insertion was left
+        {
+            if (height(t->left->left) >= height(t->left->right))
+                rotateWithLeftChild(t); // case 1 (outside)
+            else
+                doubleWithLeftChild(t); // case 2 (inside)
+        }
+        else if (height(t->right) - height(t->left) > ALLOWED_IMBALANCE) // unbalancing insertion was right
+        {
+            if (height(t->right->right) >= height(t->right->left))
+                rotateWithRightChild(t); // case 4 (outside)
+            else
+                doubleWithRightChild(t); // case 3 (inside)
+        }
+        // else ... no imbalance was created
+
+        // update height
+        t->height = max(height(t->left), height(t->right)) + 1;
+    }
+
+    int max(int lhs, int rhs) const
+    {
+        return lhs > rhs ? lhs : rhs;
+    }
+
+    /**
+     * Rotate binary tree node with left child.
+     * For AVL trees, this is a single rotation for case 1.
+     * Update heights, then set new root.
+     */
+    void rotateWithLeftChild(AvlNode *&k2)
+    {
+#ifdef DEBUG
+        cout << "need to rotateWithLeftChild for node " << k2->key << endl;
+        cout << "Before:" << endl;
+        prettyPrintTree();
+#endif
+
+        AvlNode *k1 = k2->left;
+        k2->left = k1->right;
+        k1->right = k2;
+        k2->height = max(height(k2->left), height(k2->right)) + 1;
+        k1->height = max(height(k1->left), k2->height) + 1;
+        k2 = k1;
+#ifdef DEBUG
+        cout << "After:" << endl;
+        prettyPrintTree();
+#endif
+    }
+
+    /**
+     * Rotate binary tree node with right child.
+     * For AVL trees, this is a single rotation for case 4.
+     * Update heights, then set new root.
+     */
+    void rotateWithRightChild(AvlNode *&k1)
+    {
+#ifdef DEBUG
+        cout << "need to rotateWithRightChild for node " << k1->key << endl;
+        cout << "Before:" << endl;
+        prettyPrintTree();
+
+#endif
+
+        AvlNode *k2 = k1->right;
+        k1->right = k2->left;
+        k2->left = k1;
+        k1->height = max(height(k1->left), height(k1->right)) + 1;
+        k2->height = max(height(k2->right), k1->height) + 1;
+        k1 = k2;
+#ifdef DEBUG
+        cout << "After:" << endl;
+        prettyPrintTree();
+#endif
+    }
+
+    /**
+     * Double rotate binary tree node: first left child.
+     * with its right child; then node k3 with new left child.
+     * For AVL trees, this is a double rotation for case 2.
+     * Update heights, then set new root.
+     */
+    void doubleWithLeftChild(AvlNode *&k3)
+    {
+#ifdef DEBUG
+        cout << "doubleWithLeftChild" << endl;
+#endif
+        rotateWithRightChild(k3->left);
+        rotateWithLeftChild(k3);
+    }
+
+    /**
+     * Double rotate binary tree node: first right child.
+     * with its left child; then node k1 with new right child.
+     * For AVL trees, this is a double rotation for case 3.
+     * Update heights, then set new root.
+     */
+    void doubleWithRightChild(AvlNode *&k1)
+    {
+#ifdef DEBUG
+        cout << "doubleWithRightChild" << endl;
+#endif
+        rotateWithLeftChild(k1->right);
+        rotateWithRightChild(k1);
+    }
+
+#ifdef DEBUG
+    /**
+     * Check if the tree is balanced and that the height of the nodes is correct.
+     * Throws an exception if the tree is not balanced or the height is wrong.
+     * This function is not necessary in production code since the tree is always balanced.
+     * It is only compiled when DEBUG is defined.
+     */
+
+    int check_balance(AvlNode *node)
+    {
+        if (node == nullptr)
+        {
+            return -1;
+        }
+
+        // use LRN traversal to check the balance of the tree
+        int lHeight = check_balance(node->left) + 1;
+        int rHeight = check_balance(node->right) + 1;
+
+        if (std::abs(lHeight - rHeight) > ALLOWED_IMBALANCE)
+        {
+            throw std::runtime_error("tree is not balanced in node with key " + std::to_string(node->key) + ".");
+        }
+
+        int trueNodeHeight = std::max(lHeight, rHeight);
+
+        if (trueNodeHeight != node->height)
+        {
+            throw std::runtime_error("node does not have correct height value in node with key " + std::to_string(node->key) + ".");
+        }
+
+        return trueNodeHeight;
+    }
+#endif
+};
+
+#endif
+
---
+++ test_DSAvl_tree.cpp
@@ -0,0 +1,51 @@
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+#include "DSAvl_tree.h"
+#include <iostream>
+using namespace std;
+
+
+/**
+ * most of the implementation is directly from the notes, which are right by default, so I am only testing for the new stuff I added.
+ * I added getKeys(), getValues(), and insertValue()
+*/
+
+TEST_CASE("Checks that DSAvl_tree works", "[DSAvl_tree]"){
+    
+    // setting up the tree with some test values
+    DSAvl_tree<string, int> testTree;
+    string test1 = "test1";
+    string test2 = "test2";
+    string test3 = "test3";
+    
+    testTree.insert(test1);
+    testTree.insert(test2);
+    testTree.insert(test3);
+
+    // tests getKeys(), which uses LNR to return a vector
+    vector<string> keysVector = testTree.getKeys();
+    
+    string theKeysAsStr = "";
+
+    for(int i = 0; i < keysVector.size(); i++){
+        if(i == keysVector.size() - 1){
+            theKeysAsStr += keysVector.at(i);
+            break;
+        }
+        theKeysAsStr += keysVector.at(i) + ", ";
+    }
+
+    REQUIRE(theKeysAsStr == "test1, test2, test3");
+    
+    // tests insertValue() and getValues()
+    int value1 = 1;
+    int value2 = 2;
+
+    testTree.insertValue(test1, value1);
+    testTree.insertValue(test1, value2);
+
+    vector<int> valueVector = testTree.getValues(test1);
+
+    REQUIRE(valueVector.at(0) == 1);
+    REQUIRE(valueVector.at(1) == 2);
+}
